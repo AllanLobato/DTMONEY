49cb3ab6dc8220931f7d4e6aaf20ae2df5623fd7
AllanLobato <allandonizetti@gmail.com>
2022-07-06 23:07:57 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {  // Utilizar export function define o nome do componente no export, dessa forma fica mais dificil acontecer erros
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
